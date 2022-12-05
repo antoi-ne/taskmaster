72351ebe8823ed9846390d05123aa0d8459b5cc8
Antoine Coulon <antoine@30c27b.com>
2022-12-05 23:35:33 +0100

    added documentation on the package program

--- program/doc.go
+++ program/doc.go
@@ -1,2 +1,8 @@
-// Package program represents the execution and monitoring of multiple executions of the same command.
+// Package program manages individual tasks according to a predefined configuration.
+//
+// It is capable of:
+// - retrying to start the process if it exits less than a certain time after startup;
+// - automatically decide to restart the task or not depending on its exit code;
+// - receive the instruction to be started, stopped of restarted;
+// - providing real-time informations on the task's state;
 package program
