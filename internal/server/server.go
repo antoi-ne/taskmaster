9617f3fdd648b48e609ca9cab4bec5399152ad8d
Antoine Coulon <antoine@coulon.dev>
2022-12-06 14:12:28 +0100

    added documentation to internal/manager packageg

--- internal/server/server.go
+++ internal/server/server.go
@@ -14,6 +14,7 @@ type server struct {
 	m *manager.Manager
 }
 
+// Run opens a unix socket on the given socket and serves the internal/manager package through a gRPC service.
 func Run(socket string, m *manager.Manager) error {
 	s := grpc.NewServer()
 	pb.RegisterSupervisorServer(s, &server{
