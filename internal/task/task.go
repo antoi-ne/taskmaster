ddc2a120d9978cc5167bf18523290630899d8f49
Antoine Coulon <antoine@coulon.dev>
2022-12-10 11:43:52 +0100

    working on a linux-specific implementation of task to handle Umasks

--- internal/task/task.go
+++ internal/task/task.go
@@ -2,7 +2,6 @@ package task
 
 import (
 	"errors"
-	"fmt"
 	"os"
 	"sync"
 	"time"
@@ -37,31 +36,7 @@ type Task struct {
 
 // Start a new process and monitor its status.
 func Start(a Attr, exitChan chan int) (*Task, error) {
-	t := new(Task)
-
-	t.Attr = a
-	t.exitChan = exitChan
-
-	fds, err := t.createChildFds()
-	if err != nil {
-		return nil, fmt.Errorf("could not open fds on /dev/null: %w", err)
-	}
-
-	p, err := os.StartProcess(t.Bin, t.Argv, &os.ProcAttr{
-		Dir:   t.Dir,
-		Env:   t.Env,
-		Files: fds,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("task process could not be started: %w", err)
-	}
-	t.proc = p
-
-	t.startTime = time.Now()
-
-	go t.monitor()
-
-	return t, nil
+	return start(a, exitChan)
 }
 
 // Pid returns the process ID of the task's underlying process.
